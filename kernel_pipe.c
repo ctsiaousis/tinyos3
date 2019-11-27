744bbb9a0afe16e0568c245f46839ede4aa35148
kalikanjaro <ctsiaous@gmail.com>
2019-11-27 16:08:38 +0200

    solinakia v1

--- kernel_pipe.c
+++ kernel_pipe.c
@@ -1,6 +1,7 @@
 
 #include "tinyos.h"
 #include "kernel_pipe.h"
+#include "kernel_sched.h"
 
 
 int nothing(void* this, char *buf, unsigned int size){
@@ -11,7 +12,8 @@ int nothingConst(void* this, const char *buf, unsigned int size){
 	return -1;
 }
 
-int pipe_read(void* this, char *buf, unsigned int size){
+int pipe_read(void* this, char *buf, unsigned int size)
+{
 	pipe_CB* pipeCB = (pipe_CB*) this;
 
 	//if invalid pipe_cb return -1
@@ -20,15 +22,35 @@ int pipe_read(void* this, char *buf, unsigned int size){
 	}
 
 	//if reader is closed return -1
+	if(pipeCB->pit.read == NOFILE){
+		return -1;
+	}
 
 	int count = 0;
-	while(count < size){
 
+	//an o readPTR ftasei ton writePTR kane sleep sto hasData
+	while((pipeCB->readPTR + 1)%BUF_SIZE == pipeCB->writePTR && pipeCB->pit.write != NOFILE){
+		kernel_wait(&(pipeCB->hasData), SCHED_PIPE);
 	}
+
+	//an o writer exei kanei close diabase mexri telous
+
+	//as diabasoume to solinaki
+	while(count < size && count != BUF_SIZE){
+		//an omos o reader paei na diabasi kati pou den exei grafei, ksou
+		if((pipeCB->readPTR + 1)%BUF_SIZE == pipeCB->writePTR)
+			break;
+		buf[count] = pipeCB->buffer[pipeCB->readPTR];
+		pipeCB->buffer[pipeCB->readPTR] = '\0';
+		pipeCB->readPTR = (pipeCB->readPTR + 1) % BUF_SIZE;
+		count++;
+	}
+	kernel_broadcast(&(pipeCB->hasSpace));
 	return count;
 }
 
-int pipe_write(void* this, const char* buf, unsigned int size){
+int pipe_write(void* this, const char* buf, unsigned int size)
+{
 	pipe_CB* pipeCB = (pipe_CB*) this;
 
 	//if invalid pipe_cb return -1
@@ -36,30 +58,54 @@ int pipe_write(void* this, const char* buf, unsigned int size){
 		return -1;
 	}
 
-	//if reader is closed return -1
+	//if writer or reader is closed return -1
+	if(pipeCB->pit.write == NOFILE || pipeCB->pit.read == NOFILE ){
+		return -1;
+	}
 
 	int count = 0;
-	while(count < size){
 
+	//an o writePTR ftasei ton readPTR kane sleep sto hasSpace
+	while((pipeCB->writePTR + 1)%BUF_SIZE == pipeCB->readPTR){
+		kernel_wait(&(pipeCB->hasSpace), SCHED_PIPE);
 	}
+
+	//as grapsoume sto solinaki
+	while(count < size && count != BUF_SIZE){
+		//an omos o writer paei na grapsei kati pou den diabasame ksou
+		if((pipeCB->writePTR + 1)%BUF_SIZE == pipeCB->readPTR)
+			break;
+
+		/*bale ston buffer tou pipe stin thesi tou wrPTR 
+		tin thesi counter tou buf tou orismatos */
+		pipeCB->buffer[pipeCB->writePTR] = buf[count];
+		//auksise ton writePTR ston bounded buffer
+		pipeCB->writePTR = (pipeCB->writePTR + 1) % BUF_SIZE;
+		//auksise ton counter
+		count++;
+	}
+	//pes kana koimismeno pos egrapses
+	kernel_broadcast(&(pipeCB->hasData));
 	return count;
 }
 
-int reader_Close(void* streamobj){
-
+int reader_Close(void* streamobj)
+{
+//kane pipeCB->pit.read = NOFILE
 	return -1;
 }
 
-int writer_Close(void* streamobj){
-	
+int writer_Close(void* streamobj)
+{
+//kane pipeCB->pit.write = NOFILE
 	return -1;
 }
 
 int sys_Pipe(pipe_t* pipe)
 {
 	//topikes metablites gia fid fcb kai pipeControlBlock
-	Fid_t fid[1];
-	FCB* fcb[1];
+	Fid_t fid[2];
+	FCB* fcb[2];
 	pipe_CB* cb = (pipe_CB*) xmalloc(sizeof(pipe_CB));
 
 	//kane reserve 2 FCBs
@@ -84,7 +130,8 @@ int sys_Pipe(pipe_t* pipe)
 	fcb[0]->streamobj = cb;
 	fcb[1]->streamobj = cb;
 	fcb[0]->streamfunc = &reader_ops;
-	fcb[1]->streamfunc = &reader_ops;
+	fcb[1]->streamfunc = &writer_ops;
+	//ola kalws
 	return 0;
 }
 
